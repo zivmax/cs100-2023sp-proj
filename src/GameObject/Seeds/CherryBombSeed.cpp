36d3ed59fd1a997ac15f5d06124ad1996bdc9a3f
ziv <108971427+zivmax@users.noreply.github.com>
2023-06-05 16:20:02 +0800

    Fixed the wrong imageID of the seed cards.

--- src/GameObject/Seeds/CherryBombSeed.cpp
+++ src/GameObject/Seeds/CherryBombSeed.cpp
@@ -1,4 +1,4 @@
 #include "Seeds.hpp"
 
 CherryBombSeed::CherryBombSeed(int x, pGameWorld belonging_world)
-    : SeedBase(belonging_world, IMGID_SEED_WALLNUT, ObjectOnHands::CHERRY_BOMB_SEED, 1200, 150, x) {}
+    : SeedBase(belonging_world, IMGID_SEED_CHERRY_BOMB, ObjectOnHands::CHERRY_BOMB_SEED, 1200, 150, x) {}
