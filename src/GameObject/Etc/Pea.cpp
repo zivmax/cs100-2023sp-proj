692667232fab0ed8f10fdcb131e98c6fb53f17f1
ziv <108971427+zivmax@users.noreply.github.com>
2023-06-12 09:03:04 +0800

    Fix potential bug of pea's row's caculation

--- src/GameObject/Etc/Pea.cpp
+++ src/GameObject/Etc/Pea.cpp
@@ -3,7 +3,10 @@
 static const int SPEED = 8;
 
 Pea::Pea(int x, int y, pGameWorld belonging_world)
-    : AttackingObj(belonging_world, IMGID_PEA, ANIMID_NO_ANIMATION, 20, x, y, 28, 28) {}
+    : AttackingObj(belonging_world, IMGID_PEA, ANIMID_NO_ANIMATION, 20, x, y, 28, 28) 
+{
+    m_row_on_lawn = (y - 20 - FIRST_ROW_CENTER) / LAWN_GRID_HEIGHT + 1;
+}
 
 void Pea::Update()
 {
